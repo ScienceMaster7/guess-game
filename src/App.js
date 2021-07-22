128065345f3c7ad3c62fda1249425552b38167ad
Benedikt Walch <benx.bw@gmail.com>
2021-07-22 09:19:00 +0200

    Adition of country and gender page

--- src/App.js
+++ src/App.js
@@ -8,35 +8,102 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <div className="App">
-        <header className="header">
-          <form className="header__form">
-            <input
-              className="form__input"
-              type="text"
-              placeholder="Guess your age"
-              autoComplete="off"
-            />
-            <button className="form__button">Go</button>
-          </form>
-        </header>
-        <main className="content">
-          <section className="content__output"></section>
-        </main>
-        <footer className="footer">
-          <div className="footer__iconContainer">
-            <p>AGE</p>
-          </div>
-          <div className="footer__iconContainer">
+      <footer className="footer">
+        <div className="footer__iconContainer">
+          <Link className="footer__iconContainer__link" to="/age">
+            AGE
+          </Link>
+        </div>
+        <div className="footer__iconContainer">
+          <Link className="footer__iconContainer__link" to="/country">
             <FontAwesomeIcon icon={faFlag} />
-          </div>
-          <div className="footer__iconContainer">
+          </Link>
+        </div>
+        <div className="footer__iconContainer">
+          <Link className="footer__iconContainer__link" to="/gender">
             <FontAwesomeIcon icon={faVenusMars} />
-          </div>
-        </footer>
-      </div>
+          </Link>
+        </div>
+      </footer>
+      <Switch>
+        <Route path="/country">
+          <Country />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/gender">
+          <Gender />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/age">
+          <Age />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 
+function Country() {
+  return (
+    <div className="App">
+      <header className="header">
+        <form className="header__form">
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Guess your Country"
+            autoComplete="off"
+          />
+          <button className="form__button">Go</button>
+        </form>
+      </header>
+      <main className="content">
+        <section className="content__output"></section>
+      </main>
+    </div>
+  );
+}
+
+function Gender() {
+  return (
+    <div className="App">
+      <header className="header">
+        <form className="header__form">
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Guess your Gender"
+            autoComplete="off"
+          />
+          <button className="form__button">Go</button>
+        </form>
+      </header>
+      <main className="content">
+        <section className="content__output"></section>
+      </main>
+    </div>
+  );
+}
+
+function Age() {
+  return (
+    <div className="App">
+      <header className="header">
+        <form className="header__form">
+          <input
+            className="form__input"
+            type="text"
+            placeholder="Guess your age"
+            autoComplete="off"
+          />
+          <button className="form__button">Go</button>
+        </form>
+      </header>
+      <main className="content">
+        <section className="content__output"></section>
+      </main>
+    </div>
+  );
+}
 export default App;
