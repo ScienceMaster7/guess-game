cb7a0d035c69afe82512f2a96904a8ae2ff449e2
Benedikt Walch <benx.bw@gmail.com>
2021-07-22 08:17:00 +0200

    BLayout styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFlag } from "@fortawesome/free-solid-svg-icons";
+import { faVenusMars } from "@fortawesome/free-solid-svg-icons";
 import "./App.css";
 
 function App() {
@@ -7,18 +10,29 @@ function App() {
     <Router>
       <div className="App">
         <header className="header">
-          <input
-            className="header__input"
-            type="text"
-            placeholder="Guess your age"
-            autoComplete="off"
-          />
+          <form className="header__form">
+            <input
+              className="form__input"
+              type="text"
+              placeholder="Guess your age"
+              autoComplete="off"
+            />
+            <button className="form__button">Go</button>
+          </form>
         </header>
         <main className="content">
           <section className="content__output"></section>
         </main>
         <footer className="footer">
-          <div className="footer__iconContainer"></div>
+          <div className="footer__iconContainer">
+            <p>AGE</p>
+          </div>
+          <div className="footer__iconContainer">
+            <FontAwesomeIcon icon={faFlag} />
+          </div>
+          <div className="footer__iconContainer">
+            <FontAwesomeIcon icon={faVenusMars} />
+          </div>
         </footer>
       </div>
     </Router>
